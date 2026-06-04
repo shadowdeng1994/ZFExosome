c3028200381462d7ae73546f530ac9a11c148e4e
AryaXiang <xxstarkarya@gmail.com>
2026-06-04 15:07:08 +0800

    callSNPs.sh

--- Code/Preprocess/callSNPs.sh
+++ Code/Preprocess/callSNPs.sh
@@ -4,21 +4,30 @@
 SM_PATH=$1
 SM_ID=$2
 REF="/home/xianghm/projects/chimera/genome/Danio_rerio.GRCz11.fa"
-outdir=$3
+outdir="/home/xianghm/projects/chimera/240126_combineALLvcf/genome_gVCF"
 
+
+MAX_PARALLEL=10
+
+JAVA_OPTS="-Xmx8G -XX:ParallelGCThreads=1"
 ###########################################################################
 
-gatk4 HaplotypeCaller -R ${REF} -I $SM_PATH --intervals MT --standard-min-confidence-threshold-for-calling 30 -A AlleleFraction -ERC GVCF -O ${outdir}/${SM_ID}.MT.vcf.gz &
 
-chroms=$(seq 1 25)
+chroms="MT $(seq 1 25)"
 echo ${chroms}
 echo "$SM_ID 1-25 start time: $(date)"
-for chr in ${chroms}
-do
-        echo ${chr}
-        gatk4 HaplotypeCaller -R ${REF} -I $SM_PATH --intervals ${chr} --standard-min-confidence-threshold-for-calling 30 -A AlleleFraction -ERC GVCF -O ${outdir}/${SM_ID}.$chr.vcf.gz &
-done 2>>$outdir/${SM_ID}.log2
-wait
-echo "chrom 1-25 done"
-echo "$SM_ID end time: $(date)"
+
+echo "$chroms" |tr ' ' '\n' |xargs -I {chr} -P ${MAX_PARALLEL} bash -c "
+	echo ' -> 正在处理 $SM_ID 染色体：{chr}'
+	gatk4 --java-options '${JAVA_OPTS}' HaplotypeCaller \
+		-R ${REF} \
+		-I ${SM_PATH} \
+		-L {chr} \
+		--standard-min-confidence-threshold-for-calling 30 \
+		-A AlleleFraction \
+		-ERC GVCF \
+		-O ${outdir}/${SM_ID}.{chr}.vcf.gz \
+		2>$outdir/${SM_ID}.log2
+"
+echo "[$SM_ID] 所有染色体处理完毕，结束时间：$(date)"
 
